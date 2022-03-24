d553499885533dd49cc28cc6947e291fd2c14c6b
Daniel Kuntz <kuntzd99@gmail.com>
2022-03-24 15:09:31 -0500

    CoasterDetails implemented

--- client/src/components/RideCard.js
+++ client/src/components/RideCard.js
@@ -1,8 +1,12 @@
 const RideCard = (props) => {
   return (
-    <div>
-      <h3>{props.name}</h3>
-      {/* <p>{props.description}</p> */}
+    <div clasName="card" onClick={props.onClick}>
+      <div className="img-wrapper">
+        <img src={props.image} alt="image" />
+      </div>
+      <div className="info-wrapper flex-col">
+        <h3>{props.name}</h3>
+      </div>
     </div>
   )
 }
