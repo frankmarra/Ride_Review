a77aaed1383f39e92e4ca88f18458349bd33ff50
milywendland <milynicwendland@gmail.com>
2022-03-24 14:22:34 -0600

    added some CSS

--- client/src/components/RideCard.js
+++ client/src/components/RideCard.js
@@ -2,7 +2,7 @@ const RideCard = (props) => {
   return (
     <div>
       <h3>{props.name}</h3>
-      {/* <p>{props.description}</p> */}
+      <img src={props.image} alt={props.name}></img>
     </div>
   )
 }
