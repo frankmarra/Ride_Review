39ff3a6689e3fc70d4ee9277d7daf69258a4ad70
Frank Marra <frankrmarra@gmail.com>
2022-03-24 11:06:44 -0400

    added height,seed,design to Ride model

--- server/models/Ride.js
+++ server/models/Ride.js
@@ -5,9 +5,11 @@ const Ride = new Schema(
   {
     name: { type: String, required: true },
     type: { type: String, required: true },
+    design: {type: String, required: true},
     location: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
-    duration: { type: Integer, required: true },
-    image: { type: String, requird: true },
+    height: { type: Integer, required: false },
+    speed: { type: Integer, required: false },
+    image: { type: String, required: false },
     review: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
   },
   { timestamps: true }
