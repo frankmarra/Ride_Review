e64e66366635151798816aec27543cf087e0b14c
Frank Marra <frankrmarra@gmail.com>
2022-03-24 14:03:52 -0400

    updated controller and routes for db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const routes = require('./routes')
+const db = require('./db')
 const cors = require('cors')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
-const db = require('./db')
 
 const app = express()
 
@@ -10,10 +11,8 @@ app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 
-app.get('/', (req,res)=> {
-  res.send('This is root!')
-})
+app.use('/api', routes)
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
 })
