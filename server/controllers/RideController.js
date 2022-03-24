3f273d4c323b22a092e2650b0fbd67dc8e7554f7
Frank Marra <frankrmarra@gmail.com>
2022-03-24 16:03:48 -0400

    find coaster by id added to server

--- server/controllers/RideController.js
+++ server/controllers/RideController.js
@@ -1,4 +1,4 @@
-const Ride = require('../models/Ride')
+const { Ride } = require('../models/index')
 
 const createRide = async (req, res) => {
   try {
@@ -21,7 +21,20 @@ const getAllRides = async (req, res) => {
   }
 }
 
+const getRide = async (req, res) => {
+  try {
+    const ride = await Ride.findById(req.params.id)
+    if (ride) {
+      return res.status(200).json({ ride })
+    }
+    return res.status(404).send('Ride with specified ID does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
   createRide,
-  getAllRides
+  getAllRides,
+  getRide
 }
--- server/models/Review.js
+++ server/models/Review.js
@@ -3,7 +3,7 @@ const { Schema } = require('mongoose')
 const Review = new Schema(
   {
     userName: { type: String, required: true },
-    coasterName: { type: String, required: true },
+    coasterName: { type: Schema.Types.ObjectId, ref: 'Ride', required: true },
     rating: { type: String, required: true },
     review: { type: String, required: true }
   },
