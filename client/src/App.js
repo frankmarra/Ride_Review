ec20d77937ea599c747df40499132e1e60600d1c
Frank Marra <frankrmarra@gmail.com>
2022-03-25 01:50:47 -0400

    Added parks in the nav bar to see coasters for those parks

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom'
 import Review from './pages/Review'
 import Rides from './pages/Rides'
 import CoasterDetails from './components/CoasterDetails'
+import Carowinds from './pages/Carowinds'
+import GreatAdventure from './pages/GreatAdventure'
+import Valleyfair from './pages/Valleyfair'
+import CedarPoint from './pages/CedarPoint'
 
 function App() {
   return (
@@ -16,6 +20,10 @@ function App() {
             element={<CoasterDetails />}
           />
           <Route path="/review/:rideId" element={<Review />} />
+          <Route path="/Carowinds" element={<Carowinds />} />
+          <Route path="/Great_Adventure" element={<GreatAdventure />} />
+          <Route path="/Valleyfair" element={<Valleyfair />} />
+          <Route path="/Cedar_Point" element={<CedarPoint />} />
         </Routes>
       </main>
     </div>
--- client/src/pages/GreatAdventure.js
+++ client/src/pages/GreatAdventure.js
@@ -3,16 +3,26 @@ import RideCard from '../components/RideCard'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-const Rides = () => {
+const GreatAdventure = () => {
+  const [location, setLocation] = useState()
   const [rides, setRides] = useState([])
 
   useEffect(() => {
-    const getRides = async () => {
-      const response = await axios.get('http://localhost:3001/api/rides')
-      setRides(response.data.rides)
+    const getAllLocations = async () => {
+      const response = await axios.get('http://localhost:3001/api/locations')
+      setLocation(response.data.locations[1]._id)
     }
-    getRides()
+    getAllLocations()
   }, [])
+  useEffect(() => {
+    const getRidesByPark = async () => {
+      const response = await axios.get(
+        `http://localhost:3001/api/locations/rides/${location}`
+      )
+      setRides(response.data.parkCoasters)
+    }
+    getRidesByPark()
+  }, [location])
 
   let navigate = useNavigate()
 
@@ -22,7 +32,7 @@ const Rides = () => {
 
   return (
     <div>
-      <h2>All of the coasters!</h2>
+      <h2>Rides At Great Adventure</h2>
       <section className="container">
         <div className="wrapper">
           {rides.map((ride) => (
@@ -40,4 +50,4 @@ const Rides = () => {
   )
 }
 
-export default Rides
+export default GreatAdventure
